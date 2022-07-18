a4a7887705b496394e483cdf29bdecdf70a21548
woo-chang <clllickme@naver.com>
2022-07-18 23:09:38 +0900

    아이템 28 배열보다는 리스트를 사용하라

---
+++ EffectiveJava/Chapter04/Item28/src/ArrayMain.java
@@ -0,0 +1,12 @@
+package Chapter04.Item28.src;
+
+public class ArrayMain {
+
+  public static void main(String[] args) {
+    Object[] objectArr = new Long[1];
+    // 런타임에 실패한다.
+    // ArrayStoreException
+    objectArr[0] = "타입이 다르다!";
+  }
+
+}
---
+++ EffectiveJava/Chapter04/Item28/src/ListMain.java
@@ -0,0 +1,15 @@
+package Chapter04.Item28.src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListMain {
+
+  public static void main(String[] args) {
+    // 호환되지 않는 타입
+    // List<Object> objectList = new ArrayList<Long>();
+    // 컴파일이 되지 않는다.
+    // objectList.add("타입이 다르다!");
+  }
+
+}
